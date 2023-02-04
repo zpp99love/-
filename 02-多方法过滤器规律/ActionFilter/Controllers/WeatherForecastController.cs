b410482ece3f6e00104c94a812eeb691900e114b
zhangyife2020 <1599857331@qq.com>
2023-02-04 17:57:17 +0800

    多方法过滤器的奇妙

---
+++ 02-多方法过滤器规律/ActionFilter/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            Console.WriteLine("执行Action中");
+
+            //定义异常
+            //string str = null;
+            //string str1 = str.ToString();
+           
+            Console.WriteLine("能不能走到这？");
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        }
+    }
+}
---
+++ 02-多方法过滤器规律/Infrastructure/MyActionFilter2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class MyActionFilter2 : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Console.WriteLine("MyActionFilter2前代码");
+            //定义异常
+            //string str = null;
+            //string str1 = str.ToString();
+            ActionExecutedContext result = await next();
+            //定义异常
+            //string str = null;
+            //string str1 = str.ToString();
+            if (result.Exception != null)
+            {
+                Console.WriteLine($"MyActionFilter2后代码检测到异常，该异常来自上一个过滤器或Action，异常为：{result.Exception}");
+            }
+            else
+            {
+                Console.WriteLine("MyActionFilter2前代码执行完成");
+            }
+        }
+    }
+}
