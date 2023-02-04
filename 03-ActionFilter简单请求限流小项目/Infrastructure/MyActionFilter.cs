d1c0e98610d301ae4061ccd87583cd2736852e76
zhangyife2020 <1599857331@qq.com>
2023-02-04 18:46:14 +0800

    请求限流

---
+++ 03-ActionFilter简单请求限流小项目/Infrastructure/MyActionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class MyActionFilter : IAsyncActionFilter
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public MyActionFilter(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            string ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            string cacheKey = ip;
+            long? lastTime = memoryCache.Get<long?>(cacheKey);
+
+            //判断是否1秒内多次请求
+            if (lastTime == null || Environment.TickCount64-lastTime>1000)
+            {
+                memoryCache.Set(cacheKey, Environment.TickCount64,TimeSpan.FromSeconds(10));
+                await next();         
+            }
+            else
+            {
+                ObjectResult result = new ObjectResult("你访问太频繁了") { StatusCode = 429 };
+                context.Result = result;
+            }
+        }
+    }
+}
