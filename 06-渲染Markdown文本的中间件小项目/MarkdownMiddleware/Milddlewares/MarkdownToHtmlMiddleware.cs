a7c9f397e285b912014c03039060429f149e80ff
zhangyife2020 <1599857331@qq.com>
2023-02-05 15:27:11 +0800

    markdown中间件

---
+++ 06-渲染Markdown文本的中间件小项目/MarkdownMiddleware/Milddlewares/MarkdownToHtmlMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MarkdownMiddleware.Milddlewares
+{
+    public class MarkdownToHtmlMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IWebHostEnvironment hostEn;
+
+        public MarkdownToHtmlMiddleware(RequestDelegate next, IWebHostEnvironment hostEn)
+        {
+            this.next = next;
+            this.hostEn = hostEn;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.ToString();
+            var file = hostEn.WebRootFileProvider.GetFileInfo(path);
+            if (!file.Exists)
+            {
+                //不存在则交给其他中间件处理
+                await next.Invoke(context);
+                //就不走该中间件的后逻辑了
+                return;
+            }
+            //是md文件才处理
+            if (!path.EndsWith(".md",true,null))
+            {
+                await next.Invoke(context);
+                return;
+            }
+
+            //把Markdown流变成Markdown字符串：
+                using var stream = file.CreateReadStream();
+                //安装Ude.NetStandard包，推断文件编码格式
+                Ude.CharsetDetector cdet = new Ude.CharsetDetector();
+                cdet.Feed(stream);
+                cdet.DataEnd();
+                //如果推断不出则默认为UTF-8
+                string charset = cdet.Charset ?? "UTF-8";
+                //之前推断的时候读了，现在复位
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream, Encoding.GetEncoding(charset));
+                string mdText = await reader.ReadToEndAsync();
+
+
+            //Markdown字符串转化html：
+                //安装MarkdownSharp包，将md文本转化为html
+                MarkdownSharp.Markdown md = new MarkdownSharp.Markdown();
+                string html = md.Transform(mdText);
+                context.Response.ContentType = "Text/html;charset=UTF-8";
+                await context.Response.WriteAsync(html);
+
+
+
+        }
+    }
+}
---
+++ 06-渲染Markdown文本的中间件小项目/MarkdownMiddleware/wwwroot/git.md
@@ -0,0 +1,25 @@
+```shell
+# git提示“warning: LF will be replaced by CRLF” 或
+# The file will have its original line endings in your working directory
+
+#https://blog.csdn.net/yeshenliaosuifeng/article/details/105250873
+#https://blog.csdn.net/u012757419/article/details/105614028
+```
+
+
+
+```shell
+# Reinitialized existing Git repository in C:/Users/张跑跑/Desktop/-/.git/
+
+因为该路径中已经存在.git文件，不需要再通过git init或其他方法来生成.get文件初始化仓库了
+#https://blog.csdn.net/Vest_er/article/details/127509826
+```
+
+
+
+```shell
+# OpenSSL SSL_read: Connection was reset, errno 10054
+问题原因：网络不稳定，连接超时导致。
+#https://blog.csdn.net/qq_29493173/article/details/114534057
+```
+
